5ba8a64fbc002d78962ddf4eb95d72889efcf86c
marko <markonius@pm.me>
2024-12-06 21:52:07 +0100

    Fixed stupid code

--- app/src/main/kotlin/markonius/aoc2024/Day6.kt
+++ app/src/main/kotlin/markonius/aoc2024/Day6.kt
@@ -16,53 +16,22 @@ object Day6 {
 			}
 		}
 
-		board[position] = '^'
+		board[position] = 'X'
 
-		fun directionToChar(direction: Vector2) = when(direction) {
-			Vector2.up -> '^'
-			Vector2.right -> '>'
-			Vector2.down -> 'v'
-			Vector2.left -> '<'
-			else -> throw Exception()
-		}
-
-		fun charToDirection(char: Char) = when(char) {
-			'^' -> Vector2.up
-			'>' -> Vector2.right
-			'v' -> Vector2.down
-			'<' -> Vector2.left
-			else -> throw Exception()
-		}
-
-		fun step(): Boolean {
+		fun step() {
 			if (board[position + direction] != '#') {
 				position += direction
-				if (board[position] == directionToChar(direction)) {
-					return false
-				}
 				if (board[position] == '.') {
-					board[position] = directionToChar(direction)
+					board[position] = 'X'
 					steps++
 				}
 			} else {
 				direction = direction.rotateClockwise()
 			}
-			return true
 		}
 
-		while (
-			board.isInBounds(position)
-			&& step()
-		) {
-		}
-
-		val outputFile = File("output.txt")
-		outputFile.writeText("")
-		for (j in 0 .. board.height) {
-			for (i in 0 .. board.width) {
-				outputFile.appendText(board[i, board.height - j].toString())
-			}
-			outputFile.appendText("\n")
+		while (board.isInBounds(position)) {
+			step()
 		}
 
 		return steps.toString()
